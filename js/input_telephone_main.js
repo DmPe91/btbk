baf616b96d12d1c4b4d6d2a0c68efe5b11dd6de8
DmPe91 <dmper67@gmail.com>
2024-09-11 23:45:38 +0300

    Create input_telephone_main.js

---
+++ js/input_telephone_main.js
@@ -0,0 +1,71 @@
+const input = document.querySelector("#phone");
+      const test =  window.intlTelInput(input, {
+          utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.10/build/js/utils.js",
+          initialCountry : "ru",
+          separateDialCode: true,
+          
+        });
+        const input2 = document.querySelector("#phone2");
+        window.intlTelInput(input2, {
+          utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.10/build/js/utils.js",
+          initialCountry : "ru",
+          separateDialCode: true,
+         
+        });
+        const input3 = document.querySelector("#phone3");
+        window.intlTelInput(input3, {
+          utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.10/build/js/utils.js",
+          initialCountry : "ru",
+          separateDialCode: true,
+         
+        });
+        const input_modal = document.querySelector("#phone_modal");
+        window.intlTelInput(input_modal, {
+          utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.10/build/js/utils.js",
+          initialCountry : "ru",
+          separateDialCode: true,
+         
+        });
+        
+        
+     
+
+
+
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+
+
+
+     
+     
+     
+     
+     
+     
+     
+     
